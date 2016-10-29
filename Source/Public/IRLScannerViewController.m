5b8d62e418f9465947bef37b03acd29a7e3e79f2
Barrett Jacobsen <admin@barrettj.com>
2016-10-29 11:14:28 -0500

    cleanup

--- Source/Public/IRLScannerViewController.m
+++ Source/Public/IRLScannerViewController.m
@@ -235,28 +235,10 @@
 #pragma mark - CameraVC Capture Image
 
 - (IBAction)captureButton:(id)sender {
-//    UIImage *lastCorrectedImage = [self.cameraView latestCorrectedUIImage];
-    
-    // Getting a Preview
-//    UIImageView *imgView = [[UIImageView alloc] initWithImage:lastCorrectedImage];
-//    imgView.frame = self.cameraView.frame;
-//    imgView.contentMode = UIViewContentModeScaleAspectFit;
-//    imgView.backgroundColor = [UIColor clearColor];
-//    imgView.opaque = NO;
-//    imgView.alpha = 0.0f;
-//    imgView.transform = CGAffineTransformMakeScale(0.4f, 0.4f);
-//    [self.view addSubview:imgView];
-//    
-//    [UIView animateWithDuration:0.8f delay:0.5f usingSpringWithDamping:0.3f initialSpringVelocity:0.7f options:UIViewAnimationOptionCurveEaseInOut animations:^{
-//        imgView.transform = CGAffineTransformMakeScale(0.9f, 0.9f);
-//        imgView.alpha = 1.0f;
-//
-//    } completion:nil];
-    
-    // Some Feedback to the User
     UIView *white = [[UIView alloc] initWithFrame:self.view.frame];
-    [white setBackgroundColor:[UIColor whiteColor]];
+    white.backgroundColor = UIColor.whiteColor;
     white.alpha = 0.0f;
+
     [self.view addSubview:white];
     [UIView animateWithDuration:0.2f animations:^{
         white.alpha = 1.0f;
